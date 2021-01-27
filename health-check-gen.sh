e11992ea5f3333d3a53b676c9a4dbf125ebf0711
ice-s <nguyentruongson93@gmail.com>
2021-01-27 16:25:34 +0700

    Update health-check-gen.sh

--- health-check-gen.sh
+++ health-check-gen.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Check sudo user
+if [[ "$EUID" -ne 0 ]]; then
+    echo "Please run as root or sudo"
+    exit 1;
+fi
+
 REPORTDATE="$(date +%d-%m-%y-%H%M)";
 DIR_LOG="/var/log/health-report/"
 if [[ ! -e $DIR_LOG ]]; then
@@ -7,7 +13,7 @@ fi
 
 #sudo yum -y install mailx
 
-sudo ./health-check.sh 1> $DIR_LOG/health-check-report-$REPORTDATE.txt 2> /dev/null
+./health-check.sh 1> $DIR_LOG/health-check-report-$REPORTDATE.txt 2> /dev/null
 echo -e "System Health Check Report For: $REPORTDATE" \
 | mailx -a /var/log/health-report/health-check-report-$REPORTDATE.txt -s \
 'System Health Check Report Attached' user@mail.com

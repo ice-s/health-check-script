7afda8d267aa99332d13597d4404adb1ae0d3d88
ice-s <nguyentruongson93@gmail.com>
2021-01-28 11:31:14 +0700

    Update health-check-cron.sh

--- health-check-cron.sh
+++ health-check-cron.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-RAM=$(free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }')
-CPU=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}')
-
+RAM=$(free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }')
+CPU=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}')
+if [ $RAM >= 80 || $CPU > 80 ]; then
+  echo "Server warning"
+else 
+  echo "Server OK"
+fi
